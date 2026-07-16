6925aeda3f3d8779aaa63f3316670a3e3bbee4a5
clrtrm <75531970+clrtrm@users.noreply.github.com>
2026-07-16 18:37:53 +0200

    feat: add DailyPrompt and Reply seeding content

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
-User.find_or_create_by!(username: 'admin') do |user|
+admin_user = User.find_or_create_by!(username: 'admin') do |user|
   user.display_name = 'Prompto Staff'
   user.email = 'admin@prompto.dev'
   user.password = 'changeme123!'
   user.role = :admin
 end
 
-5.times do |n|
+# Create multiple users
+
+users = 5.times.map do |n|
   User.find_or_create_by!(username: "user#{n}") do |user|
     user.display_name = "Test User #{n}"
     user.email = "user#{n}@prompto.dev"
@@ -15,6 +17,8 @@ end
     user.role = :member
   end
 end
+
+# Create multiple prompts
 
 prompts =
   ["What's a new skill you're learning right now?",
@@ -24,3 +28,31 @@ prompts =
    "What's your earliest memory?"]
 
 prompts.each { |p| Prompt.find_or_create_by!(body: p) }
+
+# Create a prompt of the day
+
+picnic_prompt = Prompt.find_or_create_by!(
+  body: 'What is the greatest picnic food?'
+)
+
+todays_prompt = DailyPrompt.find_or_create_by!(
+  date: Date.current
+) do |dp|
+  dp.prompt = picnic_prompt
+end
+
+# Create replies for the prompt of the day, from two different users
+
+Reply.find_or_create_by!(
+  daily_prompt: todays_prompt,
+  user: admin_user
+) do |reply|
+  reply.body = 'A really good potato salad with MUSTARD'
+end
+
+Reply.find_or_create_by!(
+  daily_prompt: todays_prompt,
+  user: users.first
+) do |reply|
+  reply.body = 'Watermelon but people eat it so fast lol'
+end
