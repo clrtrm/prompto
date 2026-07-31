4e37089ec3da4e42039da4aa1c4e3c09578a8f53
clrtrm <75531970+clrtrm@users.noreply.github.com>
2026-07-31 19:56:28 +0200

    fix: update broken test following User username becoming optional and autogenerated by Haikunator

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,7 +5,6 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   subject { build(:user) }
 
-  it { is_expected.to validate_presence_of(:username) }
   it { is_expected.to validate_uniqueness_of(:username) }
   it { is_expected.to validate_presence_of(:email) }
 
